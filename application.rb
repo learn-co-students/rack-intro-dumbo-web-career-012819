3b57dd5f5772d5a68d9c2a8e62fbe4bea5391d68
mazenswar <mazenswar@protonmail.com>
2019-02-16 08:04:53 -0500

    rack intro

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Mazen and I can't wait to be a web creator!"
     resp.finish
   end
 
